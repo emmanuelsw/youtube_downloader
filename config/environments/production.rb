a88c59dbacd98e5748c0bbe0e397054d16ed7417
EmmanuelSW <edhgt89@hotmail.com>
2017-10-10 20:14:53 -0500

    Configured ActionCable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_cable.allowed_request_origins = ["http://104.131.80.169/"]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
